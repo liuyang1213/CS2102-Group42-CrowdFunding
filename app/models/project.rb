b6845cfe0ff642653476174ab76700eb40edae18
liuyang <melody9951213@gmail.com>
2015-09-26 18:44:36 +0800

    validate

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
 	belongs_to :owner, foreign_key: :owner_id, class_name: :User
 	validates :name, presence: true, length: { maximum: 50 }
+	validates :target_amount, numericality: { only_integer: true, allow_blank: true }
 end
