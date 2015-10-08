c22397ba82efd197dee63e69ddda383463a3980d
liuyang <melody9951213@gmail.com>
2015-10-09 06:10:14 +0800

    add devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :projects, foreign_key: :owner_id
 	validates :name, presence: true, length: { maximum: 50 },
 				uniqueness: { case_sensitive: false }
