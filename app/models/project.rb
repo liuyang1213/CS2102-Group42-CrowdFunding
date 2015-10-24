69065247033e406d0ac675dc2999af08ea86d710
liuyang <melody9951213@gmail.com>
2015-10-24 11:19:08 +0800

    make amount and deadline required

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,9 +3,14 @@ class Project < ActiveRecord::Base
   has_many :fundings, class_name: :Funding
   has_many :funders, class_name: :User, through: :fundings, source: :user
   validates :name, presence: true, length: { maximum: 50 }
-  validates :target_amount, numericality: { only_integer: true,
-                                            allow_blank: true,
-                                            greater_than: 0 }
+  validates :target_amount,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              allow_blank: true,
+              greater_than: 0
+            }
+  validates :deadline, presence: true
 
   scope :order_by_date, (lambda do
     order('created_at desc')
--- db/migrate/20150926045132_create_projects.rb
+++ db/migrate/20150926045132_create_projects.rb
@@ -1,15 +1,15 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-    	t.string :name, null: false
-    	t.string :description
-    	t.datetime :deadline
-    	t.integer :target_amount
+      t.string :name, null: false
+      t.string :description
+      t.datetime :deadline, null: false
+      t.integer :target_amount, null: false
 
-    	t.integer :owner_id, null: false
-        t.foreign_key :users, column: :owner_id, dependent: :delete
+      t.integer :owner_id, null: false
+      t.foreign_key :users, column: :owner_id, dependent: :delete
 
-        t.timestamps
+      t.timestamps
     end
   end
 end
