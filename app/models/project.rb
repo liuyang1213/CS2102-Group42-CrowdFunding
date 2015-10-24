fe03aa0536e7f8344a9a96519e069b1f9df5158e
liuyang <melody9951213@gmail.com>
2015-10-24 10:15:57 +0800

    progress bar

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :owner, foreign_key: :owner_id, class_name: :User
   has_many :fundings, class_name: :Funding
+  has_many :funders, class_name: :User, through: :fundings, source: :user
   validates :name, presence: true, length: { maximum: 50 }
   validates :target_amount, numericality: { only_integer: true,
                                             allow_blank: true,
@@ -9,6 +10,10 @@ class Project < ActiveRecord::Base
   scope :order_by_date, (lambda do
     order('created_at desc')
   end)
+
+  def uniq_funders
+    funders.uniq
+  end
 
   def current_amount
     fundings.sum(:amount)
